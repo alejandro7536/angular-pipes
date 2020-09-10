cdb8694c47c70a9bebf4b708ed7729d3d874ca78
Alejandro Alas <alejandroalas7536@gmail.com>
2020-09-10 10:57:35 -0600

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Capitán América';
+  nombre2 = 'LuIs aLeJAndRO ALas';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+  activar = false;
+
+  fecha: Date = new Date();
+  idioma = 'es';
+  videoUrl = 'https://www.youtube.com/embed/GsKVPDtQWFw';
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Llegó la data');
+    }, 4500);
+  });
+
+
+
+
 }
